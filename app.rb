923442216a94323979e9ffbabcfc9d8b4b1949fa
Hakimu Jackson <hakimu@newrelic.com>
2015-08-26 21:38:48 -0700

    Updated Ruby file

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require 'json'
-require 'newrelic_rpm'
 require 'new_relic/agent/method_tracer'
+NewRelic::Agent.manual_start(:sync_startup => true)
 
 class Foo
 	include ::NewRelic::Agent::Instrumentation::ControllerInstrumentation 
 	def crash()
-		sleep(1)
+		# I think this API sends the class / method to the UI. Not sure?
 		NewRelic::Agent.notice_error(StandardError.new("Here's the error"))
+		# I'm not sure where this API is from??
+		::NewRelic::Agent.agent.error_collector.notice_error(ScriptError.new("Here's the script error!!!!"))
 	end
 	add_transaction_tracer :crash, :category => :task
 end
 
 Foo.new.crash
+::NewRelic::Agent.shutdown
